addc2fefc890d08839114f460afad7a30b0a098e
unknown <liangshuai_tec@126.com>
2022-07-25 10:32:55 +0800

    radio checkbox 值获取方式

--- lesson/2-form/index.js
+++ lesson/2-form/index.js
@@ -3,6 +3,44 @@ angular.module('myApp',[])
     $scope.data = {
         username:"",
         password:"",
-        confirmPassword:""
+        confirmPassword:"",
+        email:"",
+        url:"",
+        sex:"",
+        hobbies:[1,2]
     }
+
+    $scope.hobbies = [
+        {
+            id:1,
+            name:'玩游戏'
+        },
+        {
+            id:2,
+            name:'写代码'
+        },
+        {
+            id:3,
+            name:'睡觉'
+        },
+    ];
+
+    $scope.toggleHobbySelection = function(id){
+
+
+        var index = $scope.data.hobbies.indexOf(id);
+
+        if(index === -1){
+            $scope.data.hobbies.push(id);
+        }else{
+            $scope.data.hobbies.splice(index,1);
+        }
+
+        console.log($scope.data.hobbies);
+
+    }
+
+
+
+
 }])
